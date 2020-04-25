5190df633eef456f3413d7dc0e8302c0cfb7ae9c
Frank Valdez <fvaldez421@gmail.com>
2020-04-25 02:10:03 -0700

    updated wrappers components

--- app/components/styleUtils/AppContentWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const AppContentWrapper = styled.div`
-  display: block;
-  margin: auto;
-  height: 100%;
-  min-height: 100%;
-  max-width: 1350px;
-`;
