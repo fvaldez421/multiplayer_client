e316b524f42f3da9676aa35b25cdfa1da9c2809c
Frank Valdez <fvaldez421@gmail.com>
2020-04-30 02:02:24 -0700

    added colors

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,7 +1,10 @@
+export const BREAKPOINT_MOBILE_SMALL = 320;
+export const BREAKPOINT_MOBILE_LARGE = 480;
+export const BREAKPOINT_TABLET = 680;
+export const BREAKPOINT_DESKTOP_SMALL = 1092;
+export const BREAKPOINT_DESKTOP_LARGE = 1320;
 
-export const BREAKPOINT_MOBILE_SMALL = 320
-export const BREAKPOINT_MOBILE_LARGE = 480
-export const BREAKPOINT_TABLET = 680
-export const BREAKPOINT_DESKTOP_SMALL = 1092
-export const BREAKPOINT_DESKTOP_LARGE = 1320
-
+export const COLORS = {
+	lightblue: '#00c0ff',
+	strongBlue: '#0074ca',
+};
