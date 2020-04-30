c236201d8c4acf8c877da0eecfb5db6f5216f5be
Frank Valdez <fvaldez421@gmail.com>
2020-04-30 01:40:22 -0700

    added buttons file

---
+++ app/components/Buttons.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const BaseButton = styled.button`
+	border: none;
+	border-radius: 8px;
+	background-color: lightgrey;
+	color: #000000;
+	height: 38px;
+	line-height: 38px;
+	padding: 0 15px;
+	cursor: pointer;
+	&:hover {
+		filter: brightness(95%);
+	}
+	&:focus {
+		outline: none;
+	}
+	&:active {
+		filter: brightness(90%);
+	}
+`;
