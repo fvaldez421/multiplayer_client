bdda663c8db3b249635cc12f36c21c7d512187dc
Frank Valdez <fvaldez421@gmail.com>
2020-04-30 02:01:35 -0700

    fixed bug

--- app/hooks/useComplexState.js
+++ app/hooks/useComplexState.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-
+import { useState } from 'react';
 
 const useComplexState = (initialState = null, onUpdate) => {
-  const [state, updateState] = useState({ ...initialState });
-  const setState = updates => {
-    let updatedState = state
-    updateState(stale => {
-      updatedState = { ...stale, updates };
-      return updatedState
-    });
-    return updatedState
-  };
-  return {
-    state,
-    setState
-  }
+	const [state, updateState] = useState({ ...initialState });
+	const setState = (updates) => {
+		let updatedState = state;
+		updateState((stale) => {
+			updatedState = { ...stale, ...updates };
+			return updatedState;
+		});
+		if (typeof onUpdate === 'function') onUpdate(updatedState);
+		return updatedState;
+	};
+	return {
+		state,
+		setState,
+	};
 };
 
-export default useComplexState
+export default useComplexState;
