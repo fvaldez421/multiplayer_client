a5e618e81e1c023a0969f4689a488a07a9d22d7b
Frank Valdez <fvaldez421@gmail.com>
2020-05-09 00:41:39 -0700

    added helper methods to useComplexState

--- app/hooks/useComplexState.js
+++ app/hooks/useComplexState.js
@@ -1,19 +1,25 @@
 import { useState } from 'react';
 
-const useComplexState = (initialState = null, onUpdate) => {
+const useComplexState = (initialState = {}, onUpdate) => {
 	const [state, updateState] = useState({ ...initialState });
-	const setState = (updates) => {
+	const resetState = () => updateState({ ...initialState });
+	const forceState = (forceValue) => updateState({ ...forceValue });
+
+	const setState = (updates = {}) => {
 		let updatedState = state;
-		updateState((stale) => {
-			updatedState = { ...stale, ...updates };
+		updateState((staleState) => {
+			updatedState = { ...staleState, ...updates };
 			return updatedState;
 		});
 		if (typeof onUpdate === 'function') onUpdate(updatedState);
 		return updatedState;
 	};
+
 	return {
 		state,
 		setState,
+		resetState,
+		forceState,
 	};
 };
 
